33bc9b25cf63df0d9ff544f76413f83598b39cca
Vlad <sambipush@mail.ru>
2023-05-31 13:47:59 +0300

    .

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ window.addEventListener("DOMContentLoaded", () => {
     let gameActive = true;
     let currentPlayer = "X";
     let gameState = ["", "", "", "", "", "", "", "", ""];
+
     const winningMessage = () => `Player ${currentPlayer} has won!`;
     const drawMessage = () => `Game ended in a draw!`;
     const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
     const cols = document.querySelectorAll(".cell");
     const close = document.querySelectorAll(".close");
     const circle = document.querySelectorAll(".circle");
+    const reset = document.querySelector(".game--restart");
+    let animationId;
 
     statusDisplay.innerHTML = currentPlayerTurn();
 
@@ -16,12 +20,11 @@ window.addEventListener("DOMContentLoaded", () => {
         gameState[clickedCellIndex] = currentPlayer;
         clickedCell.innerHTML = currentPlayer;
     }
-
+    
     function changedPlayer() {
         currentPlayer = currentPlayer === "X" ? "O" : "X";
         statusDisplay.innerHTML = currentPlayerTurn();
     }
-
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -32,7 +35,6 @@ window.addEventListener("DOMContentLoaded", () => {
         [0, 4, 8],
         [2, 4, 6],
     ];
-
     function checkWin() {
         let roundWon = false;
         let win = null;
@@ -52,44 +54,40 @@ window.addEventListener("DOMContentLoaded", () => {
             statusDisplay.innerHTML = winningMessage();
             gameActive = false;
             cols.forEach((cell) => {
-                const CellIndex = parseInt(
+                const cellIndex = parseInt(
                     cell.getAttribute("data-cell-index")
                 );
-                if (win.includes(CellIndex)) {
-                    scaleElem(cell, 1.05, 1000, getRandomInt(10, 200)); 
-                }
-                if (currentPlayer === "O") {
-                    close.forEach((item, index) => {
-                        item.classList.add("active-circle");
-                        let duration = 5000 + 500 * index;
-                        translateElem(item, 160, -88, 130, duration, 1.7);
-                    });
-                    circle.forEach((item, index) => {
-                        let duration = 5000 + 500 * index;
-                        translateElem(item, 160, -88, 130, duration, 1.7);
-                    });
-                } else {
-                    circle.forEach((item, index) => {
-                        item.classList.add("active-close");
-                        let duration = 5000 + 500 * index;
-                        translateElem(item, 160, -88, 130, duration, 1.7);
-                    });
-                    close.forEach((item, index) => {
-                        let duration = 5000 + 500 * index;
-                        translateElem(item, 160, -88, 130, duration, 1.7);
-                    });
+                if (win.includes(cellIndex)) {
+                    scaleElem(cell, 1.05, 1000, 345, 386);
                 }
             });
-            return;
-        }
-        let roundDraw = !gameState.includes("");
-        if (roundDraw) {
+            if (currentPlayer === "O") {
+                close.forEach((item, index) => {
+                    item.classList.add("active-circle");
+                    let duration = 5000 + 500 * index;
+                    translateElem(item, 160, -88, 130, duration, 1.7);
+                });
+                circle.forEach((item, index) => {
+                    let duration = 5000 + 500 * index;
+                    translateElem(item, 160, -88, 130, duration, 1.7);
+                });
+            } else {
+                circle.forEach((item, index) => {
+                    item.classList.add("active-close");
+                    let duration = 5000 + 500 * index;
+                    translateElem(item, 160, -88, 130, duration, 1.7);
+                });
+                close.forEach((item, index) => {
+                    let duration = 5000 + 500 * index;
+                    translateElem(item, 160, -88, 130, duration, 1.7);
+                });
+            }
+        } else if (!gameState.includes("")) {
             statusDisplay.innerHTML = drawMessage();
             gameActive = false;
-            return;
+        } else {
+            changedPlayer();
         }
-
-        changedPlayer();
     }
 
     function handleCellClick(clickedCellEvent) {
@@ -101,40 +99,71 @@ window.addEventListener("DOMContentLoaded", () => {
         if (gameState[clickedCellIndex] !== "" || !gameActive) {
             return;
         }
+        
 
         handleCellPlayed(clickedCell, clickedCellIndex);
         checkWin();
+        if (currentPlayer === "O" && gameActive) {
+            setTimeout(() => {
+                botMove();
+            },500)
+        }
     }
 
     function resetGame() {
         gameActive = true;
         currentPlayer = "X";
         gameState = ["", "", "", "", "", "", "", "", ""];
-        close.forEach((item) => item.classList.remove("active-circle"));
-        circle.forEach((item) => item.classList.remove("active-close"));
-        statusDisplay.innerHTML = currentPlayerTurn();
-        cols.forEach((cell) => {
-            cell.innerHTML = "";
-            cell.style.transform = '';
-            cell.style.filter = '';
-            scaleElem(cell, 1, 0)
-        });
         close.forEach((item, index) => {
             let duration = 5000 + 500 * index;
+            item.classList.remove("active-circle");
             translateElem(item, 160, -88, 130, duration);
         });
         circle.forEach((item, index) => {
             let duration = 5000 + 500 * index;
+            item.classList.remove("active-close");
             translateElem(item, 160, -88, 130, duration);
         });
-    }
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
+        statusDisplay.innerHTML = currentPlayerTurn();
+        cols.forEach((cell) => {
+            cell.innerHTML = "";
+            for (let i = 0; i <= animationId; i++) {
+                cancelAnimationFrame(i);
+            }
+            cell.style.transform = "";
+            cell.style.filter = "";
+        });
     }
 
-    function scaleElem(element, scale, duration, filter ='none') {
+    function botMove() {
+        let availableMoves = [];
+        for (let i = 0; i < gameState.length; i++) {
+            if (gameState[i] === "") {
+                availableMoves.push(i);
+            }
+        }
+        const randomIndex = Math.floor(Math.random() * availableMoves.length);
+        const selectedSquare = availableMoves[randomIndex];
+        gameState[selectedSquare] = "O";
+        cols[selectedSquare].innerHTML = currentPlayer;
+        
+        if(checkWin && currentPlayer === 'O') {
+            checkWin();
+        } else {
+            currentPlayer = "X";
+        }
+    }
+    /////////// ANIMATION ////////////////
+    function scaleElem(
+        element,
+        scale,
+        duration,
+        minValue,
+        maxValue
+    ) {
         let start = null;
         let reverse = false;
+
         function step(timestamp) {
             if (!start) {
                 start = timestamp;
@@ -145,22 +174,29 @@ window.addEventListener("DOMContentLoaded", () => {
             let filterProgress = progress / duration;
             if (reverse) {
                 scaleProgress = 1 - scaleProgress;
-                filterProgress = 1 - filterProgress
+                filterProgress = 1 - filterProgress;
             }
 
-            const scaleValue = scaleProgress * (scale - 1) + 1; 
-            const filterValue = filter === 'none' ? 'none' : Math.min(Math.max(filterProgress * (386 - 345) + 345, 345), 386);
-            element.style.filter = `hue-rotate(${filterValue}deg)`
+            const scaleValue = scaleProgress * (scale - 1) + 1;
+            const filterValue = Math.min(
+                Math.max(
+                    filterProgress * (maxValue - minValue) + minValue,
+                    minValue
+                ),
+                maxValue
+            );
+
+            element.style.filter = `hue-rotate(${filterValue}deg)`;
             element.style.transform = `scale(${scaleValue})`;
             if (progress < duration) {
-                window.requestAnimationFrame(step);
+                animationId = requestAnimationFrame(step);
             } else {
                 reverse = !reverse;
                 start = null;
-                window.requestAnimationFrame(step);
+                animationId = requestAnimationFrame(step);
             }
         }
-        window.requestAnimationFrame(step);
+        animationId = requestAnimationFrame(step);
     }
 
     function translateElem(element, axisZ, axisY, axisX, duration, scale = 1) {
@@ -175,14 +211,13 @@ window.addEventListener("DOMContentLoaded", () => {
             let opacityProgress = 0;
             let transformProgress = progress / duration;
             let scaleProgress = progress / duration;
-            
+
             if (progress <= duration / 3.2) {
                 opacityProgress = (progress / (duration / 2)) * 0.2;
-                scaleProgress = (progress / (duration / 2) * 1)
+                scaleProgress = (progress / (duration / 2)) * 1;
             } else {
                 opacityProgress = (1 - progress / duration) * 0.2;
                 scaleProgress = (1 - progress / duration) * 1;
-
             }
             const scaleValue = scaleProgress * (scale - 1) + 1;
 
@@ -208,7 +243,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
         window.requestAnimationFrame(step);
     }
-
+    /////////////////////////////
     close.forEach((item, index) => {
         let duration = 5000 + 500 * index;
         translateElem(item, 160, -88, 130, duration);
@@ -219,7 +254,5 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     cols.forEach((cell) => cell.addEventListener("click", handleCellClick));
-    document
-        .querySelector(".game--restart")
-        .addEventListener("click", resetGame);
+    reset.addEventListener("click", resetGame);
 });
